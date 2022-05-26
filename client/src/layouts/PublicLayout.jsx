79cd11e53dbad2b9ec61bd8a5ec95421fb4e19e5
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-26 19:17:23 +0800

    video working but its looks beyond ugly

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PublicNav from "../Components/PublicNav";
 import Footer from "../Components/Footer";
-//import video from '../video/bloodcells.webm';
+import video from '../video/bloodcells.mp4';
 import css from "./PublicLayout.css";
 
 export default function PublicLayout(props) {
@@ -9,7 +9,11 @@ export default function PublicLayout(props) {
     <div>
       <PublicNav></PublicNav>
       <div id="page-container">
-        <div id="content-wrap">{props.children}</div>
+        <div id="content-wrap">
+        <video autoPlay muted loop id="myVideo">
+  <source src={video} type="video/mp4"/>
+</video>
+        {props.children}</div>
       </div>
 
       <Footer id="footer"></Footer>
