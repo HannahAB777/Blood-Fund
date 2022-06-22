fff13a1dd0221486bcd796f17d01997959bd19b9
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-06-22 14:26:27 +0800

    added construction sign pre presentation

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import underconst from '../images/underconst.webp';
+import css from './Resources.css';
 
 export default function Resources() {
   return (
-    <div>Resources</div>
+    <div>
+    <img className='constimg' src={underconst}></img>
+    </div>
   )
 }
